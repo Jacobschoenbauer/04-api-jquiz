03c43b3938c3b07d578400d62835127381cc49cd
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-20 12:02:45 -0500

    set timer function to go to html3

--- asset/script.js
+++ asset/script.js
@@ -1,2 +1,20 @@
-var startBut = document.querySelector(".start")
-var countTimer = document.querySelector(".timer")
+var startBut = document.querySelector(".start");
+var setTimer = document.querySelector(".timer");
+var testQuestion = document.querySelector(".question");
+
+var secondsLeft = 70;
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    setTimer.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      window.location = "index3.html";
+    }
+  }, 1000);
+}
+setTime();
+
+function actualQuestion() {
+  testQuestion.textContent = "First question";
+}
+actualQuestion();
