a1e6516af6862569408067c3386bb2dcddbeb059
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-20 12:49:23 -0500

    added all questions

--- asset/script.js
+++ asset/script.js
@@ -18,14 +18,60 @@ function setTime() {
 }
 setTime();
 
-function actualQuestion() {
+function actualQuestion1() {
   testQuestion.textContent =
-    "The condition in an if/else statement is enclosed with________.";
-buttonB1.textContent = " Quotes"
-buttonB2.textContent = " Curly Brackets"
-buttonB3.textContent = " Parathesis"
-buttonB4.textContent = " Square Brackets"
+    "Commonly used data types Do Not include";
+buttonB1.textContent = " Strings"
+buttonB2.textContent = " Booleans"
+buttonB3.textContent = " Alerts"
+buttonB4.textContent = " Numbers"
 
 }
+function actualQuestion1() {
+    testQuestion.textContent =
+      "The condition in an if/else statement is enclosed with________.";
+  buttonB1.textContent = " Quotes"
+  buttonB2.textContent = " Curly Brackets"
+  buttonB3.textContent = " Parathesis"
+  buttonB4.textContent = " Square Brackets"
+  
+  }
 
-actualQuestion();
+  function actualQuestion2() {
+    testQuestion.textContent =
+      "Arrays in Javascripit can be used to store __________";
+  buttonB1.textContent = " Numbers and Strings"
+  buttonB2.textContent = " Other Arrays"
+  buttonB3.textContent = " Booleans"
+  buttonB4.textContent = " All the above"
+  
+  }
+
+  function actualQuestion3() {
+    testQuestion.textContent =
+      "String values must be enclosed within _______ when being assigned to a variable";
+  buttonB1.textContent = " Commas"
+  buttonB2.textContent = " Curly Brackets"
+  buttonB3.textContent = " Parathesis"
+  buttonB4.textContent = " Quotes"
+  
+  }
+  function actualQuestion4() {
+    testQuestion.textContent =
+      "A very important tool used during development and debugging for printing content to the debugger is:";
+  buttonB1.textContent = " JavaScript"
+  buttonB2.textContent = " Terminal/bash"
+  buttonB3.textContent = " For loops"
+  buttonB4.textContent = " Console.log"
+  
+  }
+  function actualQuestion5() {
+    testQuestion.textContent =
+      "The condition in an if/else statement is enclosed with________.";
+  buttonB1.textContent = " Quotes"
+  buttonB2.textContent = " Curly Brackets"
+  buttonB3.textContent = " Parathesis"
+  buttonB4.textContent = " Square Brackets"
+  
+  }
+  actualQuestion1();
