a2ad637ba280eef05cddd080eaada18984770e6f
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-22 11:22:20 -0500

    set new qustions in a group and set it up for cotent to go to screen

--- asset/script.js
+++ asset/script.js
@@ -1,61 +1,84 @@
-var startBut = document.querySelector(".start");
-var setTimer = document.querySelector(".timer");
-var testQuestion = document.querySelector(".question");
-var buttonB1 = document.querySelector(".b1");
-var buttonB2 = document.querySelector(".b2");
-var buttonB3 = document.querySelector(".b3");
-var buttonB4 = document.querySelector(".b4");
-
-var secondsLeft = 70;
+const startBut = document.querySelector(".start");
+const setTimer = document.querySelector(".timer");
+const testQuestion = document.querySelector(".question");
+const buttonB1 = document.querySelector(".b1");
+const buttonB2 = document.querySelector(".b2");
+const buttonB3 = document.querySelector(".b3");
+const buttonB4 = document.querySelector(".b4");
+ 
+//set timer for project
+let secondsLeft = 70;
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
     setTimer.textContent = secondsLeft;
-    if (secondsLeft === 0) {
-      window.location = "index3.html";
+    if (secondsLeft <= 0) {
+      window.location = "scores.html";
     }
   }, 1000);
 }
+
+// make a object to set up questions get rid of functions
+const questions = [
+  {
+    query: "Commonly used data types Do Not include",
+    answers: [
+      { text: "Strings" },
+      { text: "Boolean" },
+      { text: "Alerts", correct: true },
+      { text: "Numbers" },
+    ],
+  },
+  {
+    query: "Arrays in Javascripit can be used to store __________",
+    answers: [
+      { text: "Numbers and Strings" },
+      { text: "Other Arrays" },
+      { text: "Boolean" },
+      { text: " All the above", correct: true },
+    ],
+  },
+  {
+    query:
+      "A very important tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      { text: "JavaScript" },
+      { text: "Terminal/bash" },
+      { text: "For loops",},
+      { text: "Console.log", correct: true },
+    ],
+  },
+  {
+    query:
+      "String values must be enclosed within______when being assigned to variables",
+    answers: [
+      { text: "Commas" },
+      { text: "Curly Brackets" },
+      { text: "Quotes", correct: true },
+      { text: "Parenthesis" },
+    ],
+  },
+  {
+    query: "The condition in an if/else statement is enclosed with ________",
+    answers: [
+      { text: "Quotes" },
+      { text: "Curly Brackets" },
+      { text: "Parathesis", correct: true },
+      { text: "Square Brackets" },
+    ],
+  },
+];
+ 
+//used to display the question on the screen
+function askQuestion (index){
+  const question = questions[index]
+    testQuestion.textContent = question.query;
+    buttonB1.textContent = question.answers[0].text;
+    buttonB2.textContent =  question.answers[1].text;
+    buttonB3.textContent = question.answers[2].text;
+    buttonB4.textContent = question.answers[3].text;
+  
+}
+
 setTime();
-
-var question1 = function actualQuestion1() {
-  testQuestion.textContent = "Commonly used data types Do Not include";
-  buttonB1.textContent = " Strings";
-  buttonB2.textContent = " Booleans";
-  buttonB3.textContent = " Alerts";
-  buttonB4.textContent = " Numbers";
-}
-function actualQuestion2() {
-  testQuestion.textContent =
-    "The condition in an if/else statement is enclosed with________.";
-  buttonB1.textContent = " Quotes";
-  buttonB2.textContent = " Curly Brackets";
-  buttonB3.textContent = " Parathesis";
-  buttonB4.textContent = " Square Brackets";
-}
-
-function actualQuestion3() {
-  testQuestion.textContent =
-    "Arrays in Javascripit can be used to store __________";
-  buttonB1.textContent = " Numbers and Strings";
-  buttonB2.textContent = " Other Arrays";
-  buttonB3.textContent = " Booleans";
-  buttonB4.textContent = " All the above";
-}
-
-function actualQuestion4() {
-  testQuestion.textContent =
-    "String values must be enclosed within _______ when being assigned to a variable";
-  buttonB1.textContent = " Commas";
-  buttonB2.textContent = " Curly Brackets";
-  buttonB3.textContent = " Parathesis";
-  buttonB4.textContent = " Quotes";
-}
-function actualQuestion5() {
-  testQuestion.textContent =
-    "A very important tool used during development and debugging for printing content to the debugger is:";
-  buttonB1.textContent = " JavaScript";
-  buttonB2.textContent = " Terminal/bash";
-  buttonB3.textContent = " For loops";
-  buttonB4.textContent = " Console.log";
-}
+askQuestion(0)
